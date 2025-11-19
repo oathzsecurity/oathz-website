5b04314adcbeb783e904c86be32abc1bb8f8bfaf
oathzsecurity <jcallis.professional@gmail.com>
2025-11-19 15:42:58 +1100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@700;800&family=Inter:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+
       <body>{children}</body>
     </html>
   );
 }
+
